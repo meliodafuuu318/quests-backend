7b5baefc59c229fc1dfe99d645c1085e4bc5c052
meliodafuuu318 <156494654+meliodafuuu318@users.noreply.github.com>
2025-11-17 15:17:53 +0800

    add register route to api

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,16 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\{
+    AuthController
+};
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::group([
+    'prefix' => 'auth'
+], function ($route) {
+    $route->post('/register', [AuthController::class, 'register']);
+});

fe6862b4558a0afb54761b39870050e26ba1e214
meliodafuuu318 <156494654+meliodafuuu318@users.noreply.github.com>
2025-11-17 15:17:35 +0800

    create auth controller and requests

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,8 +3,56 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\DB;
+use App\Http\Requests\Auth\{
+    RegisterRequest
+};
+use App\Models\User;
 
 class AuthController extends Controller
 {
-    //
+    public function register(RegisterRequest $request) {
+        if ($request->filled(['username', 'email', 'password'])) {
+            DB::beginTransaction();
+
+            try {
+                $newUser = User::create([
+                    'username' => $request->username,
+                    'email' => $request->email,
+                    'password' => Hash::make($request->password)
+                ]);
+
+                DB::commit();
+
+                return $this->success('User created successfully', $newUser);
+
+            } catch (\Exception $e) {
+                DB::rollback();
+                return $this->error('User creation failed');
+            }
+        } else {
+            return $this->error('Incomplete credentials');
+        }
+    }
+
+    public function login() {
+        //
+    }
+
+    public function logout() {
+        //
+    }
+
+    public function getAccountInfo() {
+        //
+    }
+
+    public function editAccountInfo() {
+        //
+    }
+
+    public function showUser() {
+        //
+    }
 }
--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('username');
+            $table->string('username')->unique();
             $table->decimal('exp', 12,2)->default(0);
             $table->unsignedSmallInteger('level')->default(1);
             // $table->unsignedBigInteger('avatar_id')->nullable();
